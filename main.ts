17181e0e2c09844d527f1171da3b912034dde327
manuelcasas <128051762+manuelcasas23@users.noreply.github.com>
2023-04-12 16:17:42 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("tarea 10 - sensor de sonido")
+})
 basic.forever(function () {
     basic.showNumber(input.soundLevel())
 })
 basic.forever(function () {
-    if (input.soundLevel() >= 100) {
-        for (let index = 0; index < 3; index++) {
-            music.playMelody("C5 B C5 B C5 B C5 B ", 200)
-        }
+    if (input.soundLevel() <= 255 && input.soundLevel() >= 250) {
+        music.playMelody("C5 B C5 B C5 B C5 B ", 200)
+    } else if (input.soundLevel() < 250 && input.soundLevel() >= 200) {
+        music.playMelody("A B A B A B A B ", 200)
+    } else if (input.soundLevel() < 200 && input.soundLevel() >= 150) {
+        music.playMelody("A G A G A G A G ", 200)
+    } else if (input.soundLevel() < 150 && input.soundLevel() >= 100) {
+        music.playMelody("F G F G F G F G ", 200)
+    } else if (input.soundLevel() < 100 && input.soundLevel() >= 50) {
+        music.playMelody("F E F E F E F E ", 200)
+    } else if (input.soundLevel() < 50 && input.soundLevel() >= 0) {
+        music.playMelody("D E D E D E D E ", 200)
     }
 })
