08bf22466c914525decd2fa380786d67f911d484
manuelcasas <128051762+manuelcasas23@users.noreply.github.com>
2023-04-12 16:21:25 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,16 +6,16 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (input.soundLevel() <= 255 && input.soundLevel() >= 250) {
-        music.playMelody("C5 B C5 B C5 B C5 B ", 200)
+        music.playMelody("C5 B C5 B C5 B C5 B ", 600)
     } else if (input.soundLevel() < 250 && input.soundLevel() >= 200) {
-        music.playMelody("A B A B A B A B ", 200)
+        music.playMelody("A B A B A B A B ", 500)
     } else if (input.soundLevel() < 200 && input.soundLevel() >= 150) {
-        music.playMelody("A G A G A G A G ", 200)
+        music.playMelody("A G A G A G A G ", 400)
     } else if (input.soundLevel() < 150 && input.soundLevel() >= 100) {
-        music.playMelody("F G F G F G F G ", 200)
+        music.playMelody("F G F G F G F G ", 300)
     } else if (input.soundLevel() < 100 && input.soundLevel() >= 50) {
         music.playMelody("F E F E F E F E ", 200)
     } else if (input.soundLevel() < 50 && input.soundLevel() >= 0) {
-        music.playMelody("D E D E D E D E ", 200)
+        music.playMelody("D E D E D E D E ", 100)
     }
 })
